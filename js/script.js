b0657bfd25b261c7e8d76f1a1fef65dfce992d17
Dmitr <dmitry.sytnyk@gmail.com>
2022-02-12 02:10:49 +0200

    submitting forms by xhr

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Timer
 
-  const deadline = '2022-02-09';
+  const deadline = '2022-06-13';
 
   function getTimeRemaining(endtime) {
     const t = Date.parse(endtime) - Date.parse(new Date()),
@@ -209,4 +209,56 @@ document.addEventListener('DOMContentLoaded', () => {
     '.menu .container',
     'menu__item'
   ).render();
+
+  // Forms
+
+  const forms = document.querySelectorAll('form');
+  const message = {
+    loading: 'Загрузка...',
+    success: 'Спасибо! Скоро с вами свяжемся',
+    failure: 'Что-то пошло не так..'
+  };
+
+  forms.forEach(item => {
+    postData(item);
+  });
+
+  function postData(form) {
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      const statusMessage = document.createElement('div');
+      statusMessage.classList.add('status');
+      statusMessage.textContent = message.loading;
+      form.append(statusMessage);
+
+      const request = new XMLHttpRequest();
+      request.open('POST', 'server.php');
+
+      request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+      const formData = new FormData(form);
+      
+      const object = {};
+      formData.forEach(function(value, key){
+        object[key] = value;
+      });
+
+      const json = JSON.stringify(object);
+
+      request.send(json);
+
+      request.addEventListener('load', () => {
+        if (request.status === 200) {
+          console.log(request.response);
+          statusMessage.textContent = message.success;
+          form.reset();
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 2000);
+        } else {
+          statusMessage.textContent = message.failure;
+        }
+      });
+    });
+  }
 });
